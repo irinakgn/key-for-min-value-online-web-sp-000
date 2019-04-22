d10b9f5c3e9e371e4ccdf159dd0b826d98c9d77b
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-22 23:53:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  key, value = hash.first
+  key, value = name_hash.first
   m = ''
   name_hash.each do |name, age|
     if value < age
